ff5158f341e865d9a3074d1f79db1972acb26bc5
Zack Berry <zackb123@hotmail.com>
2021-10-27 13:30:21 -0400

    Delete axon/arch_x86_old directory

--- axon/arch_x86_old/bootstrap/bsp_main.asm
+++
@@ -1,7 +0,0 @@
-;======================================================================
-;   Axon Kernel - Bootstrap Processor Main
-;   2021, Zachary Berry
-;   axon/arch_x86_64/bootstrap/bsp_main.asm
-;======================================================================
-
-;; TODO: Remove this file!
